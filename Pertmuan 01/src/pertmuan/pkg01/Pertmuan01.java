e530f3081b6c6ed85d611b3efe13136601cdb1f7
PC-F-23 <linvianti380@gmail.com>
2025-03-04 21:38:41 +0800

    Project Pertemuan 1

---
+++ Pertmuan 01/src/pertmuan/pkg01/Pertmuan01.java
@@ -0,0 +1,17 @@
+
+package pertmuan.pkg01;
+
+/**
+ *
+ * @author Linvianti
+ */
+public class Pertmuan01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Belajar Bahasa Java");
+    }
+    
+}
